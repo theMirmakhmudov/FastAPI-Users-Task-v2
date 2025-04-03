6278fd8fb17253362a5b6d3915c2fade92dc762c
multicode <mr.mirmakhmudov16112008@gmail.com>
2025-04-03 17:31:57 +0500

    Commit Last with global database

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ DB_PASSWD = os.getenv('DB_PASSWD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 
-# DATABASE_URL = f"postgres://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-DATABASE_URL = 'postgresql://neondb_owner:npg_cg7aQHGX1RxE@ep-broad-voice-a1pj6w14-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
+DATABASE_URL = f"postgres://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = 'postgresql://neondb_owner:npg_cg7aQHGX1RxE@ep-broad-voice-a1pj6w14-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
